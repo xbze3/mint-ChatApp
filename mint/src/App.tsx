cb0d48ed56e590de76539a237f7a43ce469b0120
Ezra Minty <ezranminty@gmail.com>
2025-01-02 23:33:58 -0400

    Add login form comp (Upedited)

--- mint/src/App.tsx
+++ mint/src/App.tsx
@@ -1,14 +1,16 @@
 import NavBar from "./components/NavBar";
 import ConversationAndMessageSet from "./components/ConversationAndMessageSet";
+import LoginForm from "./components/LoginForm";
 import { ConversationProvider } from "./components/special/ConversationContext";
 
 function App() {
     return (
         <>
-            <NavBar />
+            <LoginForm />
+            {/* <NavBar />
             <ConversationProvider>
                 <ConversationAndMessageSet />
-            </ConversationProvider>
+            </ConversationProvider> */}
         </>
     );
 }
---
+++ mint/src/components/LoginForm.tsx
@@ -0,0 +1,96 @@
+import "../components-css/LoginForm.css";
+import { ChangeEvent, FormEvent, useState } from "react";
+import logo from "../assets/mintLogo1.svg";
+
+function LoginForm() {
+    const [details, setDetails] = useState({
+        email_username: "",
+        password: "",
+    });
+    const [error, setError] = useState<string | null>(null);
+
+    const handleCheck = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setDetails((prev) => ({ ...prev, [name]: value }));
+    };
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+            const response = await fetch("http://localhost:8081/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(details),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+
+                localStorage.setItem("token", data.token);
+                forward(data.role);
+            } else {
+                const errorData = await response.json();
+                setError(errorData.message || "Login failed");
+            }
+        } catch (err) {
+            setError("An error occurred. Please try again.");
+        }
+    };
+
+    function forward(role: string) {
+        if (role === "Admin") {
+            window.location.href = "http://localhost:5173/admin/";
+        } else if (role === "Doctor" || role === "Nurse") {
+            window.location.href = "http://localhost:5173/med/";
+        }
+    }
+
+    return (
+        <div id="body">
+            <div>
+                <img src={logo} alt="" id="loginLogo" />
+            </div>
+            <div className="container">
+                <div className="header">
+                    <div className="text">Login</div>
+                    <div className="underline"></div>
+                </div>
+
+                {error && <div className="error-message">{error}</div>}
+
+                <form className="inputs" onSubmit={handleSubmit}>
+                    <div className="input">
+                        <input
+                            type="text"
+                            placeholder="Email / Username"
+                            name="email_username"
+                            value={details.email_username}
+                            onChange={handleCheck}
+                            required
+                        />
+                    </div>
+
+                    <div className="input">
+                        <input
+                            placeholder="Password"
+                            type="password"
+                            name="password"
+                            value={details.password}
+                            onChange={handleCheck}
+                            required
+                        />
+                    </div>
+                    <div className="submit-container">
+                        <button type="submit" className="submit">
+                            Login
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default LoginForm;
